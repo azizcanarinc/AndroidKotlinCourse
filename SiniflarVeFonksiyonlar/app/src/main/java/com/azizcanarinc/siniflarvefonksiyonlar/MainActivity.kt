413435d807f31b5a2a96473b6bd9d5bde11d7bdc
Aziz Can ARİNÇ <arincazizcan@gmail.com>
2022-08-12 22:57:58 +0300

    kotlin

--- SiniflarVeFonksiyonlar/app/src/main/java/com/azizcanarinc/siniflarvefonksiyonlar/MainActivity.kt
+++ SiniflarVeFonksiyonlar/app/src/main/java/com/azizcanarinc/siniflarvefonksiyonlar/MainActivity.kt
@@ -2,6 +2,7 @@ package com.azizcanarinc.siniflarvefonksiyonlar
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,7 +15,9 @@ class MainActivity : AppCompatActivity() {
         else{
             ikinciFonksiyon()
         }
-        cikarma(10,20)
+        cikarma(500,20)
+        var x = toplama(10,20)
+        textView.text="Sonuç : ${x}"
     }
     fun ilkFonksiyon(){
         println("ilk fonksiyon çalışıyor")
@@ -27,6 +30,10 @@ class MainActivity : AppCompatActivity() {
 
     //girdi alma
     fun cikarma(x:Int,y:Int){
-
+       textView.text="Sonuç : ${x-y}"
+    }
+    //çıktı vermek -> döndürme(return)
+    fun toplama(a:Int,b:Int):Int{
+        return a+b
     }
 }
