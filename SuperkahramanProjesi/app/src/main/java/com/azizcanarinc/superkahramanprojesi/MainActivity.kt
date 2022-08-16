3a02e28fd5985aebf1deb5910bfc83d5715aae96
Aziz Can ARİNÇ <arincazizcan@gmail.com>
2022-08-16 23:57:09 +0300

    kotlin

--- SuperkahramanProjesi/app/src/main/java/com/azizcanarinc/superkahramanprojesi/MainActivity.kt
+++ SuperkahramanProjesi/app/src/main/java/com/azizcanarinc/superkahramanprojesi/MainActivity.kt
@@ -6,17 +6,31 @@ import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private var numara = 0
+    private var isim = ""
+    private var yas : Int?= null
+    private var meslek= ""
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        numara=5
     }
 
     fun superKahramanYap(view: View){
+        numara=10
 
-        val isim= isimText.text.toString()
-        val yas= yasText.text.toString()
-        val meslek= meslekText.text.toString()
-        sonucText.text="isim: ${isim} yaş: ${yas} meslek: ${meslek}"
+         isim= isimText.text.toString()
+         yas= yasText.text.toString().toIntOrNull()
+         meslek= meslekText.text.toString()
+        if (yas==null){
+            sonucText.text= "Yaşı Doğru Girin"
+        }else {
+            var superKahraman = SuperKahraman(isim, yas!!, meslek)
+            sonucText.text =
+                "isim: ${superKahraman.isim} yaş: ${superKahraman.yas} meslek: ${superKahraman.meslek}"
+        }
 
 
     }
