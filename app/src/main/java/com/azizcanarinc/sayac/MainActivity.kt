2ea5ee14ebe3643c23276fc4fb2cd1fe98c80782
Aziz Can ARİNÇ <arincazizcan@gmail.com>
2022-09-10 23:21:55 +0300

    sayaç

--- app/src/main/java/com/azizcanarinc/sayac/MainActivity.kt
+++ app/src/main/java/com/azizcanarinc/sayac/MainActivity.kt
@@ -2,10 +2,26 @@ package com.azizcanarinc.sayac
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        object : CountDownTimer(15000,1000){
+            override fun onFinish() {
+
+                textView.text =("Kalan : 0")
+            }
+
+            override fun onTick(p0: Long) {
+
+                textView.text =("Kalan : ${p0/1000}")
+            }
+
+
+        }.start()
     }
 }
