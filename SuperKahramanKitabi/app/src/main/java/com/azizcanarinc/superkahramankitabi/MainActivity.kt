108a2016d29f1ea46f94894f9c09ad73b2669e4d
Aziz Can ARİNÇ <arincazizcan@gmail.com>
2022-09-14 22:51:03 +0300

    Süper Kahraman Kitabı

--- SuperKahramanKitabi/app/src/main/java/com/azizcanarinc/superkahramankitabi/MainActivity.kt
+++ SuperKahramanKitabi/app/src/main/java/com/azizcanarinc/superkahramankitabi/MainActivity.kt
@@ -2,10 +2,19 @@ package com.azizcanarinc.superkahramankitabi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import java.util.ArrayList
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var superKahramanIsimleri = ArrayList<String>()
+        superKahramanIsimleri.add("baydırman")
+        superKahramanIsimleri.add("Spiderman")
+        superKahramanIsimleri.add("Aquaman")
+        superKahramanIsimleri.add("Flash")
+        superKahramanIsimleri.add("İronman")
+
     }
 }
