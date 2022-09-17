07b4d3f9014c686c2acd9ad32e82eed6c0cb03e4
Aziz Can ARİNÇ <arincazizcan@gmail.com>
2022-09-17 23:52:18 +0300

    kotlin

--- SuperKahramanKitabi/app/src/main/java/com/azizcanarinc/superkahramankitabi/RecyclerAdapter.kt
+++ SuperKahramanKitabi/app/src/main/java/com/azizcanarinc/superkahramankitabi/RecyclerAdapter.kt
@@ -1,11 +1,27 @@
 package com.azizcanarinc.superkahramankitabi
 
 import android.view.View
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 
-class RecyclerAdapter : RecyclerView.Adapter<RecyclerAdapter.SuperKahramanVH>{
-    class SuperKahramanVH(itemView:View) : RecyclerView.ViewHolder() {
+class RecyclerAdapter : RecyclerView.Adapter<RecyclerAdapter.SuperKahramanVH>() {
+    class SuperKahramanVH(itemView:View) : RecyclerView.ViewHolder(itemView) {
 
     }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SuperKahramanVH {
+
+    }
+    override fun getItemCount(): Int {
+
+        
+    }
+
+
+    override fun onBindViewHolder(holder: SuperKahramanVH, position: Int) {
+
+    }
+
+
 
 }
