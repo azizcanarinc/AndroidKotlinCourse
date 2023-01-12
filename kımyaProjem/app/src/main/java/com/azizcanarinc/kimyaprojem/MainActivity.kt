bf9fe1512cdae71a70f1e2a8870e1e322724bbc0
Aziz Can ARİNÇ <arincazizcan@gmail.com>
2023-01-12 22:26:26 +0300

    kimya projem
    kimya projem

---
+++ kımyaProjem/app/src/main/java/com/azizcanarinc/kimyaprojem/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.azizcanarinc.kimyaprojem
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+    fun gurup11 (view: View){
+
+    }
+
+}
+
---
+++ kımyaProjem/app/src/main/java/com/azizcanarinc/kimyaprojem/gurup1.kt
@@ -0,0 +1,11 @@
+package com.azizcanarinc.kimyaprojem
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class gurup1 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_gurup1)
+    }
+}
