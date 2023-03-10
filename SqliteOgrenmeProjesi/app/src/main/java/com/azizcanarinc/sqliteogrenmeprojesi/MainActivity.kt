942ad38f8e4f33d9fd25336dc9eed9014387ab33
Aziz Can ARİNÇ <arincazizcan@gmail.com>
2023-03-10 23:38:26 +0300

    Sqlite
    veri çekme

--- SqliteOgrenmeProjesi/app/src/main/java/com/azizcanarinc/sqliteogrenmeprojesi/MainActivity.kt
+++ SqliteOgrenmeProjesi/app/src/main/java/com/azizcanarinc/sqliteogrenmeprojesi/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.azizcanarinc.sqliteogrenmeprojesi
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -12,8 +13,22 @@ class MainActivity : AppCompatActivity() {
 
         try {
 
-            val veritabani = this.openOrCreateDatabase()
+            val veritabani = this.openOrCreateDatabase("Urunlar", Context.MODE_PRIVATE,null)
+            veritabani.execSQL("CREATE TABLE IF NOT EXISTS urunler (id INTEGER PRIMARY KEY, isim VARCHAR, fiyat INT)")
+            //veritabani.execSQL("INSERT INTO urunler (isim,fiyat) VALUES ('Ayakkabi' ,5000)")
 
+            val cursor = veritabani.rawQuery("SELECT * FROM urunler",null)
+            val idColumnIndex = cursor.getColumnIndex("id")
+            val isimColumnIndex = cursor.getColumnIndex("isim")
+            val fiyatColumnIndex = cursor.getColumnIndex("fiyat")
+
+            while(cursor.moveToNext()){
+                println("ID : ${cursor.getInt(idColumnIndex)}")
+                println("Isim : ${cursor.getString(isimColumnIndex)}")
+                println("Fiyat : ${cursor.getInt(fiyatColumnIndex)}")
+
+            }
+            cursor.close()
 
         }catch (e : Exception){
             e.printStackTrace()
