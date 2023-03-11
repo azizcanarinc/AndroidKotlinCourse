37220c48ac6cb6214c2d1ca366bb23fa606ba602
Aziz Can ARİNÇ <arincazizcan@gmail.com>
2023-03-12 01:02:11 +0300

    yemek tarifi kitaplığı
    navigation

--- SqliteOgrenmeProjesi/app/src/main/java/com/azizcanarinc/sqliteogrenmeprojesi/MainActivity.kt
+++ SqliteOgrenmeProjesi/app/src/main/java/com/azizcanarinc/sqliteogrenmeprojesi/MainActivity.kt
@@ -14,10 +14,27 @@ class MainActivity : AppCompatActivity() {
         try {
 
             val veritabani = this.openOrCreateDatabase("Urunlar", Context.MODE_PRIVATE,null)
-            veritabani.execSQL("CREATE TABLE IF NOT EXISTS urunler (id INTEGER PRIMARY KEY, isim VARCHAR, fiyat INT)")
+            //veritabani.execSQL("CREATE TABLE IF NOT EXISTS urunler (id INTEGER PRIMARY KEY, isim VARCHAR, fiyat INT)")
             //veritabani.execSQL("INSERT INTO urunler (isim,fiyat) VALUES ('Ayakkabi' ,5000)")
+            //veritabani.execSQL("INSERT INTO urunler (isim,fiyat) VALUES ('ekrankartı' ,4000)")
+            //veritabani.execSQL("INSERT INTO urunler (isim,fiyat) VALUES ('islemci ' ,10000)")
+            //veritabani.execSQL("INSERT INTO urunler (isim,fiyat) VALUES ('kasa' ,3000)")
+            //veritabani.execSQL("INSERT INTO urunler (isim,fiyat) VALUES ('monitor' ,12000)")
+            //veritabani.execSQL("INSERT INTO urunler (isim,fiyat) VALUES ('sessistemi' ,2000)")
+            //veritabani.execSQL("INSERT INTO urunler (isim,fiyat) VALUES ('anakart' ,11000)")
+            //veritabani.execSQL("INSERT INTO urunler (isim,fiyat) VALUES ('ankrt' ,11000)")
+
+            //veritabani.execSQL("DELETE FROM urunler WHERE id = 3")
+            //veritabani.execSQL("UPDATE urunler SET fiyat = 250 where isim = 'ankrt'")
+            veritabani.execSQL("UPDATE urunler SET isim = 'monitor' where isim = 'moniror'")
+
+            //val cursor = veritabani.rawQuery("SELECT * FROM urunler WHERE isim = 'ekrankartı'",null)
+            //val cursor = veritabani.rawQuery("SELECT * FROM urunler WHERE isim LIKE '%A%'",null)
 
             val cursor = veritabani.rawQuery("SELECT * FROM urunler",null)
+
+
+
             val idColumnIndex = cursor.getColumnIndex("id")
             val isimColumnIndex = cursor.getColumnIndex("isim")
             val fiyatColumnIndex = cursor.getColumnIndex("fiyat")
